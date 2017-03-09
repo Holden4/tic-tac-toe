a73960f65001dce10962a98a9ec9b79a3aa81efb
Rob Holden <robholdenflag@gmail.com>
2017-03-09 10:52:33 +0000

    Created Player class
    Player object initializes with a name

---
+++ Gemfile
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+source "https://rubygems.org"
+
+# gem "rails"
+
+gem 'rake'
+gem 'sinatra'
+gem 'rspec-sinatra'
+gem 'capybara'
+gem 'selenium-webdriver'
+
+
+group :development, :test do
+  gem 'pry'
+  gem 'coveralls', require: false
+end
---
+++ spec/player_spec.rb
@@ -0,0 +1,19 @@
+require 'player'
+
+  describe Player do
+
+    subject(:player1) { described_class.new("Rob") }
+    subject(:player2) { described_class.new("Ben") }
+
+
+    describe '#initalize' do
+      it 'Player 1 has a name' do
+        expect(player1.name).to eq("Rob")
+      end
+
+      it 'Player 2 has a name' do
+        expect(player2.name).to eq("Ben")
+
+      end
+    end
+  end
